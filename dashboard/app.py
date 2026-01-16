d5fe75267540c472277ec4c2e4327d0db2f6a112
PandaMania <smalljsn@gmail.com>
2026-01-16 15:32:06 +0800

    going 2 weeks forward to find the remaining events for this week.

--- dashboard/app.py
+++ dashboard/app.py
@@ -200,7 +200,7 @@ df["week_end_sun_ny"] = df["close_time_ny"].dt.to_period("W-SUN").dt.end_time.dt
 now_ny = pd.Timestamp.now(tz="America/New_York")
 
 current_week_end = now_ny.to_period("W-SUN").end_time.date()
-next_week_end = (now_ny + pd.Timedelta(days=7)).to_period("W-SUN").end_time.date()
+next_week_end = (now_ny + pd.Timedelta(days=14)).to_period("W-SUN").end_time.date()
 
 df_historical = df[df["week_end_sun_ny"] <= next_week_end].copy()
 
